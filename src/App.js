49d8adde919504bf6c93180c4f560152590f45c8
kiatklai <likhitthaworn.k@gmail.com>
2023-07-23 11:43:40 +0900

    useState設定

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import {useState} from 'react'
+import {createContext, useState} from 'react'
 import Menu from './components/Menu';
 import Quiz from './components/Quiz';
 import Score from './components/Score';
 
+export const DataContext = createContext()
+
 function App() {
-  const [appState,setAppState] = useState("score")
+  const [appState,setAppState] = useState("menu")
   return (
-    <div className="App">
-      <h1>Web Development Quiz</h1>
-      {appState === "menu" && <Menu/>}
-      {appState === "quiz" && <Quiz/>}
-      {appState === "score" && <Score/>}
-    </div>
+    <DataContext.Provider value={{appState,setAppState}}>
+      <div className="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu/>}
+        {appState === "quiz" && <Quiz/>}
+        {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
   );
 }
 
