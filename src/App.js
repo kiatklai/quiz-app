c0cbb55124852bd1eb714750be14c6a2f263ebd8
kiatklai <likhitthaworn.k@gmail.com>
2023-07-23 10:45:47 +0900

    useState設定

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {useState} from 'react'
+import Menu from './components/Menu';
+import Quiz from './components/Quiz';
+import Score from './components/Score';
+
 function App() {
+  const [appState,setAppState] = useState("score")
   return (
     <div className="App">
       <h1>Web Development Quiz</h1>
+      {appState === "menu" && <Menu/>}
+      {appState === "quiz" && <Quiz/>}
+      {appState === "score" && <Score/>}
     </div>
   );
 }
